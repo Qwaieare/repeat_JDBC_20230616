5d80bf4e8ec2e9791b2d09044cbfea8bccaf4b70
Qwaieare <gb-grand@mail.ru>
2023-06-18 02:32:34 +0300

    repeat_JDBC_20230616 is done

---
+++ src/main/java/models/City.java
@@ -0,0 +1,13 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class City {
+    private int city_id;
+    private String city_name;
+}
